3149695b3dad8bb780ca134bd3b08c647baad5be
Dante Ramacciotti <danteramacciotti@pop-os.localdomain>
2024-04-14 23:58:35 -0300

    overhaul do site pra permitir todos os dados da telemetria 2024

--- telemetriaFEB/data/models.py
+++ telemetriaFEB/data/models.py
@@ -1,9 +1,42 @@
 from django.db import models
 
 class Dados(models.Model):
-    velocidade = models.FloatField()
-    temperatura_motor = models.FloatField()
-    rpm = models.IntegerField()
-    pressao_oleo = models.FloatField()
-    tensao_bateria = models.FloatField()
+    temperatura_motor = models.FloatField(null=True)
+    rpm = models.IntegerField(null=True)
+    pressao_oleo = models.FloatField(null=True)
+    tensao_bateria = models.FloatField(null=True)
+
+    mpu1_gyro_X = models.IntegerField(null=True)
+    mpu1_gyro_Y = models.IntegerField(null=True)
+    mpu1_gyro_Z = models.IntegerField(null=True)
+
+    mpu2_gyro_X = models.IntegerField(null=True)
+    mpu2_gyro_Y = models.IntegerField(null=True)
+    mpu2_gyro_Z = models.IntegerField(null=True)
+
+    mpu1_acele_X = models.FloatField(null=True)
+    mpu1_acele_Y = models.FloatField(null=True)
+    mpu1_acele_Z = models.FloatField(null=True)
+
+    mpu2_acele_X = models.FloatField(null=True)
+    mpu2_acele_Y = models.FloatField(null=True)
+    mpu2_acele_Z = models.FloatField(null=True)
+
+    pot1 = models.IntegerField(null=True)
+    pot2 = models.IntegerField(null=True)
+    pot3 = models.IntegerField(null=True)
+
+    GPSX = models.FloatField(null=True)
+    GPSY = models.FloatField(null=True)
+    GPSSpeed = models.FloatField(null=True)
+    GPSAltitude = models.FloatField(null=True)
+
+    MHPS1 = models.IntegerField(null=True)
+    MHPS2 = models.IntegerField(null=True)
+
+    MLX1 = models.FloatField(null=True)
+    MLX2 = models.FloatField(null=True)
+    MLX3 = models.FloatField(null=True)
+    MLX4 = models.FloatField(null=True)
+
     data_hora = models.DateTimeField(auto_now_add=True)
--- telemetriaFEB/data/views.py
+++ telemetriaFEB/data/views.py
@@ -8,41 +8,100 @@ from .models import Dados
 import os
 from django.conf import settings
 from django.templatetags.static import static
+from datetime import datetime, timedelta
 
 @csrf_exempt
 def receber_dados(request):
     if request.method == 'POST':
         dados_recebidos = json.loads(request.body)
         
-        velocidade = dados_recebidos['velocidade']
         temperatura_motor = dados_recebidos['temperatura_motor']
         rpm = dados_recebidos['rpm']
         pressao_oleo = dados_recebidos['pressao_oleo']
         tensao_bateria = dados_recebidos['tensao_bateria']
+
+        mpu1_gyro_Y = dados_recebidos['mpu1_gyro_Y']
+        mpu1_gyro_X = dados_recebidos['mpu1_gyro_X']
+        mpu1_gyro_Z = dados_recebidos['mpu1_gyro_Z']
+        mpu2_gyro_X = dados_recebidos['mpu2_gyro_X']
+        mpu2_gyro_Y = dados_recebidos['mpu2_gyro_Y']
+        mpu2_gyro_Z = dados_recebidos['mpu2_gyro_Z']
+
+        mpu1_acele_X = dados_recebidos['mpu1_acele_X']
+        mpu1_acele_Y = dados_recebidos['mpu1_acele_Y']
+        mpu1_acele_Z = dados_recebidos['mpu1_acele_Z']
+        mpu2_acele_X = dados_recebidos['mpu2_acele_X']
+        mpu2_acele_Y = dados_recebidos['mpu2_acele_Y']
+        mpu2_acele_Z = dados_recebidos['mpu2_acele_Z']
+
+        pot1 = dados_recebidos['pot1']
+        pot2 = dados_recebidos['pot2']
+        pot3 = dados_recebidos['pot3']
+        pot4 = dados_recebidos['pot4']
+
+        GPSX = dados_recebidos['GPSX']
+        GPSY = dados_recebidos['GPSY']
+        GPSSpeed = dados_recebidos['GPSSpeed']
+        GPSAltitude = dados_recebidos['GPSAltitude']
+
+        MHPS1 = dados_recebidos['MHPS1']
+        MHPS2 = dados_recebidos['MHPS2']
+        
+        MLX1 = dados_recebidos['MLX1']
+        MLX2 = dados_recebidos['MLX2']
+        MLX3 = dados_recebidos['MLX3']
+        MLX4 = dados_recebidos['MLX4']
+        
         
         Dados.objects.create(
-            velocidade=velocidade,
             temperatura_motor=temperatura_motor,
             rpm=rpm,
             pressao_oleo=pressao_oleo,
-            tensao_bateria=tensao_bateria
+            tensao_bateria=tensao_bateria,
+            mpu1_gyro_Y = mpu1_gyro_Y,
+            mpu1_gyro_X = mpu1_gyro_X,
+            mpu1_gyro_Z = mpu1_gyro_Z,
+            mpu2_gyro_X = mpu2_gyro_X,
+            mpu2_gyro_Y = mpu2_gyro_Y,
+            mpu2_gyro_Z = mpu2_gyro_Z,
+            mpu1_acele_X = mpu1_acele_X,
+            mpu1_acele_Y = mpu1_acele_Y,
+            mpu1_acele_Z = mpu1_acele_Z,
+            mpu2_acele_X = mpu2_acele_X,
+            mpu2_acele_Y = mpu2_acele_Y,
+            mpu2_acele_Z = mpu2_acele_Z,
+            pot1 = pot1,
+            pot2 = pot2,
+            pot3 = pot3,
+            pot4 = pot4,
+            GPSX = GPSX,
+            GPSY = GPSY,
+            GPSSpeed = GPSSpeed,
+            GPSAltitude = GPSAltitude,
+            MHPS1 = MHPS1,
+            MHPS2 = MHPS2,
+            MLX1 = MLX1,
+            MLX2 = MLX2,
+            MLX3 = MLX3,
+            MLX4 = MLX4
         )
         
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=405)
 
-def mostrar_grafico(request):
-    dados = Dados.objects.all()
-    velocidade = [d.velocidade for d in dados]
+def graficos_basicos(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
     temperatura_motor = [d.temperatura_motor for d in dados]
     rpm = [d.rpm for d in dados]
     pressao_oleo = [d.pressao_oleo for d in dados]
     tensao_bateria = [d.tensao_bateria for d in dados]
-    tempos = [d.data_hora for d in dados]
+    
     
     variaveis = [
-        [velocidade, 'velocidade'], 
         [temperatura_motor, 'temperatura_motor'], 
         [rpm, 'rpm'], 
         [pressao_oleo, 'pressao_oleo'], 
@@ -64,17 +123,204 @@ def mostrar_grafico(request):
         plt.clf()
         context[variavel[1]] = imagem_src
         
-    """plt.plot(tempos, velocidades)
-    plt.xlabel('Data e Hora')
-    plt.ylabel('Velocidade')
-    plt.title('Variação da Velocidade')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    image_path = settings.STATICFILES_DIRS[0] + 'velocidade.png'
-    velocidade_src = 'image/velocidade.png'
+    return render(request, 'grafico.html', {'context':context})
 
-    plt.savefig(image_path)  # Salva o gráfico como uma imagem
-    context = {'velocidade': velocidade_src}"""
+def graficos_MPU(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
+    mpu1_gyro_X = [d.mpu1_gyro_X for d in dados]
+    mpu1_gyro_Y = [d.mpu1_gyro_Y for d in dados]
+    mpu1_gyro_Z = [d.mpu1_gyro_Z for d in dados]
+    mpu2_gyro_X = [d.mpu2_gyro_X for d in dados]
+    mpu2_gyro_Y = [d.mpu2_gyro_Y for d in dados]
+    mpu2_gyro_Z = [d.mpu2_gyro_Z for d in dados]
+
+    mpu1_acele_X = [d.mpu1_acele_X for d in dados]
+    mpu1_acele_Y = [d.mpu1_acele_Y for d in dados]
+    mpu1_acele_Z = [d.mpu1_acele_Z for d in dados]
+    mpu2_acele_X = [d.mpu2_acele_X for d in dados]
+    mpu2_acele_Y = [d.mpu2_acele_Y for d in dados]
+    mpu2_acele_Z = [d.mpu2_acele_Z for d in dados]
+
+    variaveis =[
+        [mpu1_gyro_X, 'mpu1_gyro_X'],
+        [mpu1_gyro_Y, 'mpu1_gyro_Y'],
+        [mpu1_gyro_Z, 'mpu1_gyro_Z'],
+        [mpu2_gyro_X, 'mpu2_gyro_X'],
+        [mpu2_gyro_Y, 'mpu2_gyro_Y'],
+        [mpu2_gyro_Z, 'mpu2_gyro_Z'],
+        [mpu1_acele_X, 'mpu1_acele_X'],
+        [mpu1_acele_Y, 'mpu1_acele_Y'],
+        [mpu1_acele_Z, 'mpu1_acele_Z'],
+        [mpu2_acele_X, 'mpu2_acele_X'],
+        [mpu2_acele_Y, 'mpu2_acele_Y'],
+        [mpu2_acele_Z, 'mpu2_acele_Z']
+    ]
+
+    context = {}
     
-    return render(request, 'grafico.html', context)
+    for variavel in variaveis:
+        plt.plot(tempos, variavel[0])
+        plt.xlabel('Data e Hora')
+        plt.ylabel(variavel[1])
+        plt.title('Variação de '+ variavel[1])
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        
+        image_path = settings.STATICFILES_DIRS[0] + variavel[1] +'.png'
+        imagem_src = 'image/'+variavel[1]+'.png'
+        plt.savefig(image_path)
+        plt.clf()
+        context[variavel[1]] = imagem_src
+    
+    return render(request, 'grafico.html', {'context':context})
 
+def graficos_analogicos(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
+    pot1 = [d.pot1 for d in dados]
+    pot2 = [d.pot2 for d in dados]
+    pot3 = [d.pot3 for d in dados]
+    pot4 = [d.pot4 for d in dados]
+    MHPS1 = [d.MHPS1 for d in dados]
+    MHPS2 = [d.MHPS2 for d in dados]
+
+    variaveis = [
+        [pot1, 'pot1'],
+        [pot2, 'pot2'],
+        [pot3, 'pot3'],
+        [pot4, 'pot4'],
+        [MHPS1, 'MHPS1'],
+        [MHPS2, 'MHPS2']
+    ]
+
+    context = {}
+    
+    for variavel in variaveis:
+        plt.plot(tempos, variavel[0])
+        plt.xlabel('Data e Hora')
+        plt.ylabel(variavel[1])
+        plt.title('Variação de '+ variavel[1])
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        
+        image_path = settings.STATICFILES_DIRS[0] + variavel[1] +'.png'
+        imagem_src = 'image/'+variavel[1]+'.png'
+        plt.savefig(image_path)
+        plt.clf()
+        context[variavel[1]] = imagem_src
+    
+    return render(request, 'grafico.html', {'context':context})
+
+def graficos_GPS(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
+
+    GPSX = [d.GPSX for d in dados]
+    GPSY = [d.GPSY for d in dados]
+    GPSSpeed = [d.GPSSpeed for d in dados]
+    GPSAltitude = [d.GPSAltitude for d in dados]
+    variaveis = [
+        [GPSX, 'GPSX'],
+        [GPSY, 'GPSY'],
+        [GPSSpeed, 'GPSSpeed'],
+        [GPSAltitude, 'GPSAltitude']]
+
+
+    context = {}
+    
+    for variavel in variaveis:
+        plt.plot(tempos, variavel[0])
+        plt.xlabel('Data e Hora')
+        plt.ylabel(variavel[1])
+        plt.title('Variação de '+ variavel[1])
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        
+        image_path = settings.STATICFILES_DIRS[0] + variavel[1] +'.png'
+        imagem_src = 'image/'+variavel[1]+'.png'
+        plt.savefig(image_path)
+        plt.clf()
+        context[variavel[1]] = imagem_src
+    
+    return render(request, 'grafico.html', {'context':context})
+
+def graficos_MLX(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
+
+    MLX1 = [d.MLX1 for d in dados]
+    MLX2 = [d.MLX2 for d in dados]
+    MLX3 = [d.MLX3 for d in dados]
+    MLX4 = [d.MLX4 for d in dados]
+    variaveis = [
+        [MLX1, 'MLX1'],
+        [MLX2, 'MLX2'],
+        [MLX3, 'MLX3'],
+        [MLX4, 'MLX4']]
+
+
+    context = {}
+    
+    for variavel in variaveis:
+        plt.plot(tempos, variavel[0])
+        plt.xlabel('Data e Hora')
+        plt.ylabel(variavel[1])
+        plt.title('Variação de '+ variavel[1])
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        
+        image_path = settings.STATICFILES_DIRS[0] + variavel[1] +'.png'
+        imagem_src = 'image/'+variavel[1]+'.png'
+        plt.savefig(image_path)
+        plt.clf()
+        context[variavel[1]] = imagem_src
+    
+    return render(request, 'grafico.html', {'context':context})
+
+def index(request):
+    return render(request, 'index.html')
+
+
+"""
+def graficos_novos(request):
+    tempo_anterior = datetime.now() - timedelta(minutes=5)
+    dados = Dados.objects.filter(data_hora__gte=tempo_anterior)
+    tempos = [d.data_hora for d in dados]
+
+    var_nova1 = [d.var_nova1 for d in dados]
+    var_nova2 = [d.var_nova2 for d in dados]
+    var_nova3 = [d.var_nova3 for d in dados]
+
+    variaveis =[
+        [var_nova1, 'var_nova1'],
+        [var_nova2, 'var_nova2'],
+        [var_nova3, 'var_nova3']
+    ]
+
+    context = {}
+    
+    for variavel in variaveis:
+        plt.plot(tempos, variavel[0])
+        plt.xlabel('Data e Hora')
+        plt.ylabel(variavel[1])
+        plt.title('Variação de '+ variavel[1])
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        
+        image_path = settings.STATICFILES_DIRS[0] + variavel[1] +'.png'
+        imagem_src = 'image/'+variavel[1]+'.png'
+        plt.savefig(image_path)
+        plt.clf()
+        context[variavel[1]] = imagem_src
+    
+    return render(request, 'grafico.html', {'context':context})
+"""
